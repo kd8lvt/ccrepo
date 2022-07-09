49f10787c3e54a0627898d4fb42490564cc3ce9a
kd8lvt <kd8lvt@users.noreply.github.com>
2022-07-09 19:25:16 -0400

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,6 +1,6 @@
 files = {
     ["clay_arena"] = {
-        ["base_url"] = "http://ccrepo.kd8lvt.com/clay_arena/"
+        ["base_url"] = "http://ccrepo.kd8lvt.com/clay_arena/",
         ["computer"] = {
             ["server"] = {
                 ["main_script"] = "computer/server.lua",
@@ -12,7 +12,7 @@ files = {
                 ["dependencies"] = {},
                 ["has_dependencies"] = false
             }
-        }
+        },
         ["turtle"] = {
             ["enderchest_manager"] = {
                 ["main_script"] = "turtle/enderchest_manager.lua",
